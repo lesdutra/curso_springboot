3cf070e36ee651b5bae51b82de701fbb889e2bbc
Leandro Dutra <leandro.dutra1990@gmail.com>
2019-01-26 19:51:39 -0200

    Instanciacao automatica para poupador BD

--- src/main/java/com/treinamento/spring/CursoApplication.java
+++ src/main/java/com/treinamento/spring/CursoApplication.java
@@ -1,13 +1,32 @@
 package com.treinamento.spring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.treinamento.spring.domain.Categoria;
+import com.treinamento.spring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }

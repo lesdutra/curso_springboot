bca9b1b62f7c00f7c59cd71b398231d9222ace8b
Leandro Dutra <leandro.dutra1990@gmail.com>
2019-01-27 15:16:03 -0200

    Implementacao Insert por Method POST Categoria

--- src/main/java/com/treinamento/spring/resources/CategoriaResource.java
+++ src/main/java/com/treinamento/spring/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.treinamento.spring.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.treinamento.spring.domain.Categoria;
 import com.treinamento.spring.services.CategoriaService;
@@ -19,10 +23,15 @@ public class CategoriaResource {
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find( @PathVariable Integer id ) {
-
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert( @RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }

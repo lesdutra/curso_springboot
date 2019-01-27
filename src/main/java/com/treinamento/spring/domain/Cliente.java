3862c15ba077ee920854ac5d5de62ebcbd8ae353
Leandro Dutra <leandro.dutra1990@gmail.com>
2019-01-27 14:46:15 -0200

    Alteracao para uso do JSonIgnore

--- src/main/java/com/treinamento/spring/domain/Cliente.java
+++ src/main/java/com/treinamento/spring/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.treinamento.spring.domain.enums.TipoCliente;
 
 @Entity
@@ -30,11 +29,10 @@ public class Cliente implements Serializable {
 	private String cpfCnpj;
 	private Integer tipo;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	

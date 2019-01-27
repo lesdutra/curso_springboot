72520a0aa049da5764810d8638710e0ca0b2f072
Leandro Dutra <leandro.dutra1990@gmail.com>
2019-01-27 17:09:35 -0200

    Inserindo Paginação na pesquisa de categorias

--- src/main/java/com/treinamento/spring/services/CategoriaService.java
+++ src/main/java/com/treinamento/spring/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.treinamento.spring.domain.Categoria;
@@ -24,10 +27,6 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
-	public List<Categoria> findAll() {
-		return repo.findAll();
-	}
-	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
@@ -45,6 +44,15 @@ public class CategoriaService {
 		} catch( DataIntegrityViolationException e ) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
